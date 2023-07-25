6ea1bd1f952dd3c75356df0c9ad6055a03d6229c
rutvisvyas <140504557+rutvisvyas@users.noreply.github.com>
2023-07-25 19:00:38 +0530

    Update streamlit_app.py
    have added healthy menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
+streamlit.header('breakfast menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale , spinach and rocket smoothie')
+streamlit.text('hard-boiled free range eggs')
+
 
