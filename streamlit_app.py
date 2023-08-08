dcffa3616fb990887ecc26db429a2946d1eb8193
rutvisvyas <140504557+rutvisvyas@users.noreply.github.com>
2023-08-08 13:16:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruits_to_show1 = my_fruit_list.loc[fruityvice_response]
+fruits_to_show1 = my_fruit_list.loc[fruit_choice]
 streamlit.dataframe (fruits_to_show1)
 
 
